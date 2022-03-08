f56e86c459f61816a67c82bd9ecca9d36cb15976
SongSiWoon <siwoons7319@gmail.com>
2022-03-09 00:50:50 +0900

    Feat: Add first project

---
+++ src/main/java/com/shoecream/store/ShoeCreamApplication.java
@@ -0,0 +1,13 @@
+package com.shoecream.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeCreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoeCreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shoecream/store/ShoeCreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoecream.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeCreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
